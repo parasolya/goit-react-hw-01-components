a4decbf50fa01cab62c445c637eebb7b3ff7487a
Olga Pysarenko <ola.shevchuk@gmail.com>
2023-05-03 09:32:22 +0300

    fix title

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,7 +5,7 @@ import StatisticItem from './StatisticItem';
 function Statistics({ title, stats }) {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{title ? 'Upload stats' : ''}</h2>
+      {title &&<h2 className={css.title}>{title}</h2>}
       <ul className={css.statList}>
         {stats.map(el => (
           <StatisticItem
